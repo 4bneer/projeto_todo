17380b38fa231388933371335dac263330206482
Abner Batista <abnerjob16@gmail.com>
2024-04-10 19:39:09 -0300

    erro resolvido

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
     const button = $('button')
     const linhas = $('li')
     
-    $("#container").on("click", "li", function(e) {
-        e.preventDEfaul();
+    $("#container").on("click", "li", function() {
         $(this).toggleClass("completed")})
 
-    button.on('click', function(){
+    button.on('click', function(e){
+        e.preventDefault()
         let task = $('#tarefa').val()
         $(`<li>${task}</li>`).appendTo('#list')
         $('#tarefa').val('')
